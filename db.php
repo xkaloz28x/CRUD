84e15feb45fe5fd17481c4d6e30dd2dfb80d7eb5
xkaloz28x <117710480+xkaloz28x@users.noreply.github.com>
2025-07-08 21:40:53 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$db = 'usuarios_db';
-$user = 'root'; // o tu usuario
-$pass = '';     // o tu contraseña
+$host = 'sql202.infinityfree.com';
+$db = 'if0_39426880_usuarios_db';
+$user = 'if0_39426880'; // 
+$pass = 'contra291202';    
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
